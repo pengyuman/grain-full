af221648a67ae754458cbbe88ce300f3b3d7ab0b
Seven <45868775+pengyuman@users.noreply.github.com>
2020-10-18 22:17:41 +0800

    feat：eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a= '123'
+  if(a==='123'){
+    console.log('git ')
+  }
   return (
     <div className="App">
       <header className="App-header">
